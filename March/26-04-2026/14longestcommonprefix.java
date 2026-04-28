3d2d44aa2a0263981a4fd4ed49cb3485c6a29061
Pranav Dixit <pranavdixit512@gmail.com>
2026-04-28 19:31:14 +0545

    Add longestCommonPrefix method implementation
    Implemented a method to find the longest common prefix among an array of strings by sorting and comparing the first and last strings.

--- March/26-04-2026/14longestcommonprefix.java
+++ March/26-04-2026/14longestcommonprefix.java
@@ -18,6 +18,21 @@ Space Complexity: O(1)
 
 Pattern:
 String Traversal / Comparison
+
+class Solution {
+    public String longestCommonPrefix(String[] strs){
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int i =0;
+         while(i < first.length() && i < last.length() 
+              && first.charAt(i) == last.charAt(i)){
+            i++;
+        }
+        return first.substring(0,i);
+
+    }
+}
 */
 class Solution {
     public String longestCommonPrefix(String[] strs){
