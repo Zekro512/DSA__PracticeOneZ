63c1c604c4854c8fcf6e32a7143f134f57b5238b
Pranav Dixit <pranavdixit512@gmail.com>
2026-05-01 16:00:34 +0545

    Add solution for finding first occurrence of substring
    Implement brute force solution to find first occurrence of a substring in a string.

---
+++ May/01-05-2026/28Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,36 @@
+/*
+Platform: LeetCode
+Problem: Find the Index of First Occurrence in a String
+Date: 21-04-2026
+
+Problem:
+Find first occurrence of substring in string.
+
+Approach:
+Brute Force (String Matching)
+
+Idea:
+Check substring starting at each index.
+
+Time Complexity: O(n * m)
+Space Complexity: O(1)
+
+Pattern:
+String Matching / Sliding Comparison
+*/
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        for(int i=0; i<=n-m; i++){
+            int j = 0;
+        while(j < m && haystack.charAt(i + j) == needle.charAt(j)){
+            j++;
+            }
+            if(j==m){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
