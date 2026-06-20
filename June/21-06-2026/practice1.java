4ea772e2feb6c38f40ee749eea6a967c1254ab70
Pranav Dixit <pranavdixit512@gmail.com>
2026-06-21 00:07:21 +0545

    Add practice1.java for number checks

---
+++ June/21-06-2026/practice1.java
@@ -0,0 +1,33 @@
+// Positive Number Check
+/*import java.util.Scanner;
+public class practicee {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        if(n<0){
+            System.out.println("Negative number");
+        }else{
+            System.out.println("Positive number");
+        }
+
+    }
+}
+
+
+//2 EVEN or ODD
+import java.util.Scanner;
+public class practicee{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+
+    }
+}
