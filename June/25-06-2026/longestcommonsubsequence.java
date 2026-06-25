8939e443b03384a72c51c1f5c34e33111b5481a4
Pranav Dixit <pranavdixit512@gmail.com>
2026-06-25 10:50:28 +0545

    Fix formatting and spacing in LCS implementation

--- June/25-06-2026/longestcommonsubsequence.java
+++ June/25-06-2026/longestcommonsubsequence.java
@@ -1,17 +1,22 @@
 //brute fore 
+
 class Solution {
+
     public int longestCommonSubsequence(String text1, String text2) {
-        return solve(0,0,text1,text2);
+        return solve(0, 0, text1, text2);
     }
-    private int solve(int i, int j, String s1, String s2){
-        if(i == s1.length() || j == s2.length())
-        return 0;
-        if(s1.charAt(i) == s2.charAt(j))
-        return 1 +solve(i+i,j+1,s1,s2);
+
+    private int solve(int i, int j, String s1, String s2) {
+
+        if (i == s1.length() || j == s2.length())
+            return 0;
+
+        if (s1.charAt(i) == s2.charAt(j))
+            return 1 + solve(i + 1, j + 1, s1, s2);
+
         return Math.max(
-            solve(i+1,j,s1,s2)
-            solve(i,j+1,s1,s2)
-        )
+                solve(i + 1, j, s1, s2),
+                solve(i, j + 1, s1, s2));
     }
 }
 
