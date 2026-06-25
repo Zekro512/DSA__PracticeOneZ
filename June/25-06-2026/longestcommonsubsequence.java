47722bff777f37955d26aa0a71ebfb65743d961a
Pranav Dixit <pranavdixit512@gmail.com>
2026-06-25 10:44:52 +0545

    Add brute force algorithm for longest common subsequence
    Implement brute force solution for longest common subsequence.

---
+++ June/25-06-2026/longestcommonsubsequence.java
@@ -0,0 +1,34 @@
+//brute fore 
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        return solve(0,0,text1,text2);
+    }
+    private int solve(int i, int j, String s1, String s2){
+        if(i == s1.length() || j == s2.length())
+        return 0;
+        if(s1.charAt(i) == s2.charAt(j))
+        return 1 +solve(i+i,j+1,s1,s2);
+        return Math.max(
+            solve(i+1,j,s1,s2)
+            solve(i,j+1,s1,s2)
+        )
+    }
+}
+
+
+Recursive Relation
+If
+
+
+text1[i] == text2[j]
+Then
+
+
+1 + solve(i+1,j+1)
+Else
+
+
+max(
+solve(i+1,j),
+solve(i,j+1)
+)
