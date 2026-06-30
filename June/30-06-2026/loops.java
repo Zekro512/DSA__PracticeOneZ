4cd94b020627c6749d6745dd0628a337a87ac080
Pranav Dixit <pranavdixit512@gmail.com>
2026-06-30 23:38:13 +0545

    Add loops.java with square and triangle patterns

---
+++ June/30-06-2026/loops.java
@@ -0,0 +1,47 @@
+public class loopas {
+    //Question 1: Print Square Pattern (Easy)
+    public static void main(String[] args){
+        //Print Inverted Right Triangle
+        for(int i = 1; i<=5; i++){
+            for(int j =1; j <=6-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        /* 
+        //Print Right Triangle Pattern
+        for(int i=1; i<=5; i++){
+            for(int j =1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //q2  Print Perpendicular Line (Easy)//   
+        
+        *****
+          *
+          * 
+          * 
+          * 
+          
+         for(int i =1; i<=5; i++){
+            for(int j=1;  j<=5; j++){
+                if(i==1 || j==3){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }*/
+       /*for(int i = 1; i<=4; i++){
+            for(int j = 1; j<=4; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+    }
+
+    
+}
